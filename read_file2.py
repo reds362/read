e3052dc1738cfaae30153833492dac50160fcd73
reds362 <cbweng0924@yahoo.comtw>
2022-05-27 12:15:06 +0800

    filter

--- read_file2.py
+++ read_file2.py
@@ -5,7 +5,7 @@ with open('reviews.txt', 'r') as f:
 		
 		data.append(line)
 		count += 1
-		if count % 10000 ==0:
+		if count % 100000 ==0:
 			print(count)
 
 print(len(data))
@@ -16,4 +16,22 @@ for d in data:
 print(ch_sum)
 
 print('平均每筆有', ch_sum / len(data), '個字母')
+
+
+new =[]
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new),'筆資料小於100')
+print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good),'筆資料')
+print(good[1])
+
+
+
 
